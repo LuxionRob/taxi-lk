20bf5cdbc59d8328828b4e7ef7c7c6c7b970cc0b
LuxioRob <aidaynhi8@gmail.com>
2022-08-30 17:47:27 +0700

    Route and controller

--- src/index.js
+++ src/index.js
@@ -1,28 +1,28 @@
-const express = require('express')  
-const morgan = require('morgan')
-const { create  } = require('express-handlebars')
-const path = require('path')
-const app = express()
+const express = require('express');
+const morgan = require('morgan');
+const { create } = require('express-handlebars');
+const path = require('path');
+const app = express();
+const route = require('./routes');
 
-const port = 3000
-const hostname = '127.0.0.1'
+const port = 3000;
+const hostname = '127.0.0.1';
 
-app.use(express.static(path.join(__dirname, 'public')))
-app.use(morgan('combined'))
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(morgan('combined'));
 
 const hbs = create({
-  partialsDir   : path.join(__dirname, 'resources/views/partials'),
-  extname       : '.hbs',
-  layoutsDir    : path.join(__dirname, 'resources/views/layouts'),
-})
-app.engine('hbs', hbs.engine)
-app.set('view engine', 'hbs')
-app.set('views', path.join(__dirname, 'resources/views'))
+    partialsDir: path.join(__dirname, 'resources/views/partials'),
+    extname: '.hbs',
+    layoutsDir: path.join(__dirname, 'resources/views/layouts'),
+});
+app.engine('hbs', hbs.engine);
+app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname, 'resources/views'));
 
-app.get('/', (req, res) => {res.render('home')})
-app.get('/news', (req, res) => {res.render('news')})
-
+// Route init
+route(app);
 
 app.listen(3000, hostname, () => {
-  console.log(`Server is listening at http://${hostname}:${port}`)
-})
+    console.log(`Server is listening at http://${hostname}:${port}`);
+});
