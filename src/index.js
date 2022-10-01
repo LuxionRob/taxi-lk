2d593a1aae599ad8dde0da5d62809263ed23bef7
Luxion <45875394+LuxionRob@users.noreply.github.com>
2022-10-01 10:24:02 +0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -5,10 +5,7 @@ const path = require('path')
 const cors = require('cors')
 const app = express()
 const route = require('./routes')
-const http = require('http')
-const fs = require('fs')
 const port = process.env.PORT || 3000
-const hostname = process.env.HOST_ENV || 'localhost'
 
 app.use(express.static(path.join(process.cwd(), 'dist')))
 app.use(helmet())
@@ -18,6 +15,6 @@ app.use(morgan('combined'))
 // Route init
 route(app)
 
-app.listen(port, hostname, () => {
-  console.log(`Server is listening at http://${hostname}:${port}`)
+app.listen(port, () => {
+  console.log(`Server is listening on port ${port}`)
 })
