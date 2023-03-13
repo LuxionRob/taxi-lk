b30a859fea9bb4a45dd44859ba361438f4ca5154
Luxion Rob <aidaynhi8@gmail.com>
2023-03-13 23:38:24 +0700

    Add: robot sitemap

--- src/app/controllers/siteController.js
+++ src/app/controllers/siteController.js
@@ -3,9 +3,6 @@ class SiteController {
   index(req, res) {
     res.sendFile(path.join(__dirname, 'html/home.html'))
   }
-  sendSitemap(req, res) {
-    res.sendFile(path.join(__dirname, 'sitemap.xml'))
-  }
 }
 
 module.exports = new SiteController()
--- src/routes/site.js
+++ src/routes/site.js
@@ -2,7 +2,6 @@ const express = require('express')
 const router = express.Router()
 const siteController = require('../app/controllers/siteController')
 
-router.use('/sitemap.xml', siteController.sendSitemap)
 router.use('/', siteController.index)
 
 module.exports = router
